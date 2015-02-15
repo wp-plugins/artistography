01fd4f2c78cf513b2aea5826f576d207653ee589
MistahWrite <MistahWrite@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-15 03:07:37 +0000

    Add Lightbox and Colorbox Gallery styles as well as tagged 0.2.1-alpha5
    git-svn-id: https://plugins.svn.wordpress.org/artistography/tags/0.2.1-alpha5@1090202 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/script.js
+++ js/script.js
@@ -16,6 +16,9 @@ $(document).ready(function() {
     if ( $( "#accordion1" ).length != 0 ) $( "#accordion1" ).accordion({ autoHeight: false});
     if ( $( "#accordion2" ).length != 0 ) $( "#accordion2" ).accordion({ autoHeight: false});
     if ( $( "#accordion3" ).length != 0 ) $( "#accordion3" ).accordion({ autoHeight: false});
+    if ( $( "#accordion4" ).length != 0 ) $( "#accordion4" ).accordion({ autoHeight: false});
+
+    $(".colorbox").colorbox({scalePhotos:true, maxWidth: '90%', maxHeight: '90%', slideshow:true});
 
     $('.artist_table').slideDown();
 
