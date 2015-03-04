f9a452499958f817d59203385464365f53992bcc
MistahWrite <MistahWrite@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-04 06:57:53 +0000

    Updated tags/0.3.3-alpha
    git-svn-id: https://plugins.svn.wordpress.org/artistography/tags/0.3.3-alpha@1104776 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/script.js
+++ js/script.js
@@ -1,100 +1,230 @@
 var $ = jQuery.noConflict();
 
-
-(function($) {
-   jQuery.fn.backgroundPosition = function() {
-     var p = $(this).css('background-position');
-     if(typeof(p) === 'undefined') return $(this).css('background-position-x') + ' ' + $(this).css('background-position-y');
-     else return p;
-   };
- })(jQuery); 
-
-
 $(document).ready(function() {
     var $tabs = $('#tabs').tabs({ fx: { opacity: 'toggle', height: 'toggle', width: 'toggle' } }).slideDown();
 
     if ( $( "#accordion1" ).length != 0 ) $( "#accordion1" ).accordion({ autoHeight: false});
     if ( $( "#accordion2" ).length != 0 ) $( "#accordion2" ).accordion({ autoHeight: false});
     if ( $( "#accordion3" ).length != 0 ) $( "#accordion3" ).accordion({ autoHeight: false});
+    if ( $( "#accordion4" ).length != 0 ) $( "#accordion4" ).accordion({ autoHeight: false});
+
+	function LoadShoppingCart() {
+	    var data = {
+	        action: 'Get_Cart_Contents'
+	    };
+	    $.post(MyAjax.ajaxurl, data, function(response) {
+	    	$(".artistography_cart").empty().append(response);
+
+		 /* Delete From Cart Button */
+		$( ".remove_from_cart" ).button({
+		    text: true,
+		    icons: {
+		        primary: "ui-icon-trash"
+		    }    }).click(function() {
+		    var id = $( this ).attr('id');
+		    var data = {
+		        action: 'Remove_From_Cart',
+        	        product: id
+		    };
+		    $.post(MyAjax.ajaxurl, data, function(response) {
+		        LoadShoppingCart();
+			LoadBasicShoppingCart(false);
+        	   });
+    		});
+	    });
+	}
+	LoadShoppingCart();
+
+        function LoadBasicShoppingCart(scroll_to) {
+                if (typeof scroll_to === "undefined" || scroll_to === null) {
+                        scroll_to = true; 
+                }
+            var data = {
+                action: 'Get_Basic_Cart_Contents'
+            };
+            $.post(MyAjax.ajaxurl, data, function(response) {
+                $(".artistography_basic_cart").empty().append(response);
+                if(scroll_to) {
+                        $('html, body').animate({
+                                scrollTop: $(".widget_artistography_cart_widget").offset().top
+                        }, 2000);
+                }
+
+            });
+        }
+        LoadBasicShoppingCart(false);
+
+     /* Edit Shopping Cart Button */
+    $( ".edit_cart" ).button({
+        text: true,
+        icons: {
+            primary: "ui-icon-cart"
+        }
+    }).click(function() {
+	/* Do AJAX call to get URL of cart and go there */
+	  var data = {
+                  action: 'Get_Cart_URL'
+          };
+          $.post(MyAjax.ajaxurl, data, function(response) {
+		location.assign(response);
+          });
+    });
+
+     /* Play Song Button */
+    $( ".artistography_play" ).button({
+        text: false,
+        icons: {
+            primary: "ui-icon-play"
+        }
+    }).click(function() {
+        var id = $( this ).attr('id').replace('song_play_', '');
+          var data = {
+                  action: 'Play_Song',
+                  song_id: id
+          };
+          $.post(MyAjax.ajaxurl, data, function(response) {
+		 /* Put song or album Into soundmanager2 playing */
+		$( ".sm2-playlist-bd" ).empty().append(response);
+		var i = 0;
+		var num = 0;
+		 /* For some reason this gives me i = li * 2 */
+		$(".sm2-playlist-bd").children('li').each(function() {
+			i += 1;
+			if ( $( this ).is('.selected') ) {
+				num = i;
+			}
+		});
+		var count = i / 2;
+		num = num - count;
+		var offset = num - 1;
+
+		var item = getItem(offset);
+		select(item);
+		setTitle(item);
+		var link = getURL();
+
+		soundObject = makeSound(link);
+		soundObject.togglePause();
+          });
+    });
+
+     /* Buy Song Button */
+    $( ".song_to_cart" ).button({
+        text: true,
+        icons: {
+            primary: "ui-icon-cart"
+        }
+    }).click(function() {
+        var id = $( this ).attr('id').replace('song_download_', '');
+          var data = {
+                  action: 'Add_Song_To_Cart',
+		  song_id: id
+          };
+          $.post(MyAjax.ajaxurl, data, function(response) {
+                LoadBasicShoppingCart();
+		LoadShoppingCart();
+          });
+    });
+
+     /* Buy Album Download Button */
+    $( ".album_download_to_cart" ).button({
+        text: false,
+        icons: {
+            primary: "ui-icon-cart"
+        }
+    }).click(function() {
+        var id = $( this ).attr('id').replace('album_download_', '');
+          var data = {
+                  action: 'Add_Album_Download_To_Cart',
+                  album_id: id
+          };
+          $.post(MyAjax.ajaxurl, data, function(response) {
+		LoadBasicShoppingCart();
+                LoadShoppingCart();
+          });
+    });
+
+    $(".colorbox").colorbox({scalePhotos:true, maxWidth: '90%', maxHeight: '90%', slideshow:true});
 
     $('.artist_table').slideDown();
 
-    //Custom settings
-    var style_in = 'easeOutBounce';
-    var style_out = 'jswing';
-    var speed_in = 1000;
-    var speed_out = 300;    
+    $('.qitem')
+	.hover(
+		function() {
+			$(this).find('img').fadeOut();
+		}, function(){
+        	        $(this).find('img').fadeIn(1000);
+   		}
+    	);
 
-    //Calculation for corners
-    var neg = Math.round($('.qitem').width() / 2) * (-1);   
-    var pos = neg * (-1);   
-    var out = pos * 2;
+	soundManager.setup({
+		url: '../soundmanagerv297a-20140901/swf/',
+		flashVersion: 9, // optional: shiny features (default = 8)
+		 // optional: ignore Flash where possible, use 100% HTML5 mode
+		 //preferFlash: false,
 
-    $('.qitem').each(function () {
+		onready: function() {
+		 // Ready to use; soundManager.createSound() etc. can now be called.
+        		var link;
+			$( "a.track_length" ).each(function(index) {
+				link = $( this ).attr('href');
+				setDuration($( this ), link);
+        		});
+		}
+	});
 
-        //grab the anchor and image path
-        url = $(this).find('a').attr('href');
-        img = $(this).find('img').attr('src');
-        img_size = $(this).find('img').attr('width');
-        new_img = 'artistography/wp-content/plugins/artistography/css/images/ajax-loader.gif';
+	function msToTime(duration) {
+		duration = duration | 0;
+        	var milliseconds = parseInt((duration%1000)/100)
+        	    , seconds = parseInt((duration/1000)%60)
+        	    , minutes = parseInt((duration/(1000*60))%60)
+        	    , hours = parseInt((duration/(1000*60*60))%24);
 
-        //remove the image
-        $('img', this).remove();
+        	hours = (hours < 10) ? "0" + hours : hours;
+        	minutes = (minutes < 10) ? "0" + minutes : minutes;
+        	seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-        //append four corners/divs into it
-        $(this).append('<div class="topLeft"></div><div class="topRight"></div><div class="bottomLeft"></div><div class="bottomRight"></div>');
-         
-        if (navigator.appName=='Microsoft Internet Explorer') {
-          $(this).find('div.topLeft').append('<img src="' + img + '" style="position:absolute;height:200%;width:200%;top:0px;left:0px;" />');
-          $(this).find('div.topRight').append('<img src="' + img + '" style="border:0px;position:absolute;height:200%;width:200%;top:0px;left:' + neg + 'px;" />');
-          $(this).find('div.bottomLeft').append('<img src="' + img + '" style="border:0px;position:absolute;height:200%;width:200%;top:' + neg + 'px;left:0px;" />');
-          $(this).find('div.bottomRight').append('<img src="' + img + '" style="border:0px;position:absolute;height:200%;width:200%;top:' + neg + 'px;left:' + neg + 'px;" />');
-        } else {
-          //set the background image to all the corners
-          $(this).children('div').css('background-image','url("'+ img + '")');
-          //set the background image to all the corners
-          $(this).children('div').css('background-size','200%');
+		if (hours == "00") {
+			return minutes + ":" + seconds;
+		} else {
+        		return hours + ":" + minutes + ":" + seconds;
+		}
+    	}
+
+        function setDuration(object, url)
+        {
+                var mySound = soundManager.createSound({
+			url: url,
+			autoLoad: true,
+			whileloading: function () {
+				if (this.durationEstimate > 0) {
+					object.replaceWith(msToTime('' + this.durationEstimate + ''));
+					this.destruct();
+				}
+			},
+			onload: function() {
+				object.replaceWith(msToTime('' + this.duration + ''));
+				this.destruct();
+			}
+		});
         }
-
-        if (navigator.appName=='Microsoft Internet Explorer') {
-          //set the position of corners
-          $(this).find('div.topLeft').css({top:0, left:0, width:pos , height:pos});
-          $(this).find('div.topRight').css({top:0, left:pos, width:pos , height:pos});
-          $(this).find('div.bottomLeft').css({bottom:1, left:0, width:pos , height:pos});
-          $(this).find('div.bottomRight').css({bottom:1, left:pos, width:pos , height:pos}); 
-        } else {
-          //set the position of corners
-          $(this).find('div.topLeft').css({top:0, left:0, width:pos , height:pos});
-          $(this).find('div.topRight').css({top:0, left:pos, width:pos , height:pos});
-          $(this).find('div.bottomLeft').css({bottom:0, left:0, width:pos , height:pos});
-          $(this).find('div.bottomRight').css({bottom:0, left:pos, width:pos , height:pos}); 
-        }
-    }).hover(function () {
-        //animate the position
-        $(this).find('div.topLeft').stop(false, true).animate({top:neg, left:neg}, {duration:speed_out, easing:style_out}); 
-        $(this).find('div.topRight').stop(false, true).animate({top:neg, left:out}, {duration:speed_out, easing:style_out});    
-        $(this).find('div.bottomLeft').stop(false, true).animate({bottom:neg, left:neg}, {duration:speed_out, easing:style_out});   
-        $(this).find('div.bottomRight').stop(false, true).animate({bottom:neg, left:out}, {duration:speed_out, easing:style_out});  
-    },
-
-    function () {
-        if (navigator.appName=='Microsoft Internet Explorer') {
-          //put corners back to the original position
-          $(this).find('div.topLeft').stop(false, true).animate({top:0, left:0}, {duration:speed_in, easing:style_in});   
-          $(this).find('div.topRight').stop(false, true).animate({top:0, left:pos}, {duration:speed_in, easing:style_in});    
-          $(this).find('div.bottomLeft').stop(false, true).animate({bottom:1, left:0}, {duration:speed_in, easing:style_in}); 
-          $(this).find('div.bottomRight').stop(false, true).animate({bottom:1, left:pos}, {duration:speed_in, easing:style_in});  
-        } else {
-          //put corners back to the original position
-          $(this).find('div.topLeft').stop(false, true).animate({top:0, left:0}, {duration:speed_in, easing:style_in});   
-          $(this).find('div.topRight').stop(false, true).animate({top:0, left:pos}, {duration:speed_in, easing:style_in});    
-          $(this).find('div.bottomLeft').stop(false, true).animate({bottom:0, left:0}, {duration:speed_in, easing:style_in}); 
-          $(this).find('div.bottomRight').stop(false, true).animate({bottom:0, left:pos}, {duration:speed_in, easing:style_in});  
-        }
-    }).click (function () {
-        //go to the url
-        window.location = $(this).find('a').attr('href');
-        $(this).hide("explode", 1000);
-    });
 
 });
+
+      function loadAlbumArt(song_id) {
+          var data = {
+                  action: 'Load_Album_Art',
+                  song_id: song_id
+          };
+          $.post(MyAjax.ajaxurl, data, function(response) {
+              $( "#sm2-album-art" ).attr('src', response);
+              $( "#sm2-album-art" ).attr('height', '100%');
+	      var height = $( "#sm2-album-art" ).height();
+              $("#sm2-album-art" ).attr('width', height);
+          });
+      }
+
+      function loadAlbumArtRefresh(height) {
+              $( "#sm2-album-art" ).attr('height', height);
+              $("#sm2-album-art" ).attr('width', height);
+      }
